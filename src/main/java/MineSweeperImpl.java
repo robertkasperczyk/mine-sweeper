b891cd6bc235f9632f8e2fe706744c28d8c7667d
robertt240 <robertkasperczyk@gmail.com>
2017-09-19 19:36:23 +0200

    Initial commit

---
+++ src/main/java/MineSweeperImpl.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+
+public class MineSweeperImpl implements MineSweeper {
+    private char[][] mineField;
+
+    public char[][] getMineField() {
+        return mineField;
+    }
+
+    public void setMineField(String mineField) throws IllegalArgumentException {
+        if (mineField == null || mineField.equals("")) {
+            throw new IllegalArgumentException("Cannot process empty string and null!");
+        }
+        String[] rows = mineField.split("\n");
+
+        if (Arrays.asList(rows)
+                .stream()
+                .anyMatch(s -> s.length() != rows[0].length())) {
+            throw new IllegalArgumentException("Two rows cannot have different length!");
+        }
+        this.mineField = new char[rows.length][rows[0].length()];
+
+        for (int i = 0; i < rows.length; i++) {
+            this.mineField[i] = rows[i].toCharArray();
+        }
+
+        for (int i = 0; i < this.mineField.length; i++) {
+            for (int j = 0; j < this.mineField[i].length; j++) {
+                System.out.print(this.mineField[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public String getHintField() throws IllegalStateException {
+        if (mineField == null) {
+            throw new IllegalStateException("mine-field has not been initialised!");
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < this.mineField.length; i++) {
+            for (int j = 0; j < this.mineField[i].length; j++) {
+                if (this.mineField[i][j] == '*') {
+                    result.append(this.mineField[i][j]);
+                    continue;
+                }
+                result.append(getAdjacentMines(i, j));
+            }
+            result.append('\n');
+        }
+
+        return result.deleteCharAt(result.length()-1).toString();
+    }
+
+    private int getAdjacentMines(int i, int j) {
+        int adjacentMines = 0;
+        for (int k = -1; k < 2; k++) {
+            if (i + k < 0 || i + k > this.mineField.length - 1) {
+                continue;
+            }
+            for (int l = -1; l < 2; l++) {
+                if (j + l < 0 || j + l > this.mineField[i].length - 1) {
+                    continue;
+                }
+                if (this.mineField[i + k][j + l] == '*') {
+                    adjacentMines++;
+                }
+            }
+        }
+        return adjacentMines;
+    }
+
+}
---
+++ src/test/java/MineSweeperImplTest.java
@@ -0,0 +1,76 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MineSweeperImplTest {
+    private MineSweeperImpl mineSweeper;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mineSweeper = new MineSweeperImpl();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetMineFieldWhenNull() throws Exception {
+        //when
+        mineSweeper.setMineField(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetMineFieldWhenEmptyString() throws Exception {
+        //when
+        mineSweeper.setMineField("");
+    }
+
+    @Test()
+    public void testSetMineFieldWhenProperString() throws Exception {
+        char[][] expected = {
+                {'*', '.', '.'},
+                {'.', '.', '*'}
+        };
+
+        //given
+        String input = "*..\n..*";
+
+        //when
+        mineSweeper.setMineField(input);
+
+        //then
+        assertArrayEquals(expected, mineSweeper.getMineField());
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetMineFieldWhenImproperString() throws Exception {
+
+        //given
+        String input = "*..\n...*";
+
+        //when
+        mineSweeper.setMineField(input);
+
+    }
+
+    @Test
+    public void testGetHintFieldWithProperMineField() throws Exception {
+        //given
+        mineSweeper.setMineField("*...\n..*.\n....");
+
+        //when
+        String result = mineSweeper.getHintField();
+
+        //then
+        assertEquals("*211\n12*1\n0111", result);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testGetHintFieldWithNotInitialisedMineField() throws Exception {
+
+        //when
+        String result = mineSweeper.getHintField();
+
+    }
+
+}
